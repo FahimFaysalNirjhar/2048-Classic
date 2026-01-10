e313eeba8aa214c47e0c4a10e17f2a74a8942f81
Fahim Faysal Nirjhar <fahimfaysal1995@gmail.com>
2026-01-10 22:06:11 +0600

    slide left fuction implemented

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const tileContainer = document.getElementById("tile-container");
+const scoreValue = document.getElementById("score");
 
 let board;
 let rows = 4,
@@ -36,6 +37,51 @@ function updateTile(tile, num) {
     tile.textContent = num;
     if (num <= 4096) {
       tile.classList.add(`x${num}`);
+    } else {
+      tile.classList.add(`x8192`);
     }
   }
+}
+
+document.addEventListener("keyup", (e) => {
+  if (e.code === "ArrowLeft") {
+    slideLeft();
+  } else if (e.code === "ArrowRight") {
+    slideRight();
+  }
+});
+function slideLeft() {
+  for (let r = 0; r < rows; r++) {
+    let row = board[r];
+    row = slide(row);
+    board[r] = row;
+    for (let c = 0; c < columns; c++) {
+      let tile = document.getElementById(`${r}-${c}`);
+      let num = board[r][c];
+      updateTile(tile, num);
+    }
+  }
+}
+
+function filterZero(row) {
+  return row.filter((num) => num != 0);
+}
+
+function slide(row) {
+  row = filterZero(row);
+  console.log(row);
+
+  for (let i = 0; i < row.length - 1; i++) {
+    if (row[i] === row[i + 1]) {
+      row[i] *= 2;
+      row[i + 1] = 0;
+      score += row[i];
+      scoreValue.textContent = score;
+    }
+  }
+  row = filterZero(row);
+  while (row.lenght < columns) {
+    row.push(0);
+  }
+  return row;
 }
